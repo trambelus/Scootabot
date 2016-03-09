a33b6217425c88ff4a5f378d8398f6af3434ebab
Hawke252 <hawkemaul16@yahoo.com>
2016-03-08 20:02:42 -0500

    Updated emotes
    Added emotes enum and added list of 'hi' emotes

--- emotes.py
+++ emotes.py
@@ -1,13 +1,42 @@
 #!/usr/bin/env python3
 # em.py - contains emote definitions
 
-nope = ['3g','angryal','confidentscoots','creepingloo','cutealoo','damusics','danceofherpeople',
-	'depressedscoots','easemyscoots','evilscoot','gonnabeawesome','ibelieveicanfly','icametowritefanfics',
-	'hoo1','hoo2','iamafillyandwhatisthis','letmetellyousomething','mfilly','notquitedashie','needsmorerainbowdash',
-	'nicemoves','scaredaloo','scaredyscoots','scaredyscoots2','scootaball','scootabreak','scootabuzzoff','scootachill',
-	'scootacloak','scootacookie2','scootacool2','scootacornered','scootacrush','scootacutie','scootadodo','scootadown',
-	'scootadrop','scootaderp','scootaderp2','scootafloor','scootahey','scootamap','scootapissed','scootasad2','scootasaywhat',
-	'scootaserious','scootasmug','scootasorry','scootastare','scootasure','scootathanks','scootathink','scootatrot',
-	'scootatwirl2','scootawat','scootawhat','scootawink','scootaworried','scootperplexity','scootired','scootscared',
-	'scootcontent','scoottongue','shockedscoots','sparkaloo', 
-	'masterofdrums', 'scootastop', 'scootasurprised', 'scootuh', 'scootuhwhat', 'wetscoots'] # Unsorted, emotes Hawke added
+from enum import Enum
+
+emote = Enum('emote', 'nope bye error hi huh yep')
+
+nope = ['3g','angryal','confidentscoots','creepingloo','cutealoo','damusics',
+        'danceofherpeople','depressedscoots','easemyscoots','evilscoot',
+        'gonnabeawesome','ibelieveicanfly','icametowritefanfics','hoo1','hoo2',
+        'iamafillyandwhatisthis','letmetellyousomething','masterofdrums',
+        'mfilly','notquitedashie','needsmorerainbowdash','nicemoves',
+        'scaredaloo','scaredyscoots','scaredyscoots2','scootaball',
+        'scootabreak','scootabuzzoff','scootachill','scootacloak',
+        'scootacookie2','scootacool2','scootacornered','scootacrush',
+        'scootacutie','scootadodo','scootadown','scootadrop','scootaderp',
+        'scootaderp2','scootafloor','scootahey','scootamap','scootapissed',
+        'scootasad2','scootasaywhat','scootaserious','scootasmug','scootasorry',
+        'scootastare','scootastop','scootasure','scootasurprised',
+        'scootathanks','scootathink','scootatrot','scootatwirl2','scootawat',
+        'scootawhat','scootawink','scootaworried','scootdealwithit',
+        'scootperplexity','scootpocker','scootired','scootscared','scootcontent',
+        'scoottongue','scootuh','scootuhwhat','shockedscoots',
+        'wetscoots']
+
+bye = [ ]
+
+error = [ ]
+
+hi = ['adoorable','alandsublink','confidentscoots','creepaloo','cutealoo',
+      'danceofherpeople','eqgscoots','grinaloo','needsmorerainbowdash',
+      'scootaball','scootabat','scootabat2','scootabounce','scootabow',
+      'scootacheer','scootacookie','scootadorkable','scootahanging',
+      'scootahoodie','scootajump','scootajump3','scootaloodle','scootanoms',
+      'scootapoint2','scootarear','scootarear2','scootasit','scootasit2',
+      'scootasmile','scootasmile2','scootasmug','scootasquee','scootastand',
+      'scootasure','scootawink','scootdealwithit','smirkaloo','sparkaloo',
+      'telegram','wolfscoots','wonderscoots']
+
+huh = [ ]
+
+yep = [ ]
