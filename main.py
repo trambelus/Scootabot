5bd0960c2626f1c03dc0cd0733cc4fb9d9fe8a03
Hawke252 <hawkemaul16@yahoo.com>
2016-03-11 03:24:04 -0500

    Fix for !again command
    Added self to parameters for process call

--- main.py
+++ main.py
@@ -73,7 +73,7 @@ class Command:
 
 			if self.command.startswith('!again') and self.message.author.id in self.last_command:
 				self.command = self.last_command[self.message.author.id]
-				return process()
+				return process(self)
 
 			if re.search(r'(^| )(hi|hello|hey|hi there|hiya|heya|howdy)(! |, | )scootabot', self.command):
 				author = re.search('(^| )\w+$', self.message.author.name).group().strip().title()
