ed8b6c2fdea11314c751a0fc086f24830bff34a5
Hawke252 <hawkemaul16@yahoo.com>
2016-06-29 22:25:55 -0400

    Added Nicknames

--- response.py
+++ response.py
@@ -5,6 +5,13 @@ import re
 
 import emotes
 
+nicknames = {
+	'134968288772882432': ['Hawkaloo', 'Hawke', 'Hawkey', 'Hawkey Hawke']
+	'136190686230741002': ['Trambles', 'Trambelus', 'Trambuck', 'Buck', 'Applebuck', 'Applepone', 'Best Apple']
+	'110932722322505728': ['Woof', 'Woofy', 'Ian', 'Woofy Woof', 'Best Woof']
+	'166229158681247753': ['Carbon', 'Dragon', 'Carburd', 'Best Dragon']
+	'any': ['Sexy', 'Butts' ]
+}
 def respond(self):
 	response = ""
 
@@ -12,10 +19,14 @@ def respond(self):
 	if re.search(r'(^| )(hi|hello|hey|hi there|hiya|heya|howdy)(! |, | )scootabot', self.command):
 		#author = re.search('(^| )\w+( ♀)?$', self.message.author.name).group().strip().title()
 		#ret = emotes.get_message(emotes.HI, author)
-		if (self.message.author.id == '134968288772882432'):
-			response = "Hawkaloo... You son of a bitch"
+		chance = random.randint(0, 10)
+
+		if (chance < 7):
+			response = emotes.get_message(emotes.HI, nicknames[self.message.author.id])
+		elif (7 <= chance < 9):
+			response = emotes.get_message(emotes.HI, self.message.author.display_name)
 		else:
-			response = emotes.get_message(emotes.HI, self.message.author.id)
+			response = emotes.get_message(emotes.HI, nicknames['any'])
 	elif ("?" in self.command):
 		chance = random.randint(0, 98)
 
