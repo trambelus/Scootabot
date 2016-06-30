44b35f531515a312bfad02d002b5d0e0b35c54dc
Trambelus <trambelus2@gmail.com>
2016-06-29 21:49:21 -0400

    Fix indents

--- response.py
+++ response.py
@@ -3,18 +3,18 @@ import random
 
 import emotes
 
-	def respond(message):
-		response = ""
+def respond(message):
+	response = ""
 
-		if ("?" in message):
-			chance = random.randint(0, 98)
+	if ("?" in message):
+		chance = random.randint(0, 98)
 
-			if (chance < 34):
-				response = emotes.get_message(emotes.YEP)
-			elif (34 <= chance < 67):
-				response = emotes.get_message(emotes.NOPE)
-			else:
-				response = emotes.get_message(emotes.HUH)
+		if (chance < 34):
+			response = emotes.get_message(emotes.YEP)
+		elif (34 <= chance < 67):
+			response = emotes.get_message(emotes.NOPE)
+		else:
+			response = emotes.get_message(emotes.HUH)
 
-		return response
+	return response
 
