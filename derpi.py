ae16266e8f366758fde05f01b7b29381d0efaa29
Hawke252 <hawkemaul16@yahoo.com>
2016-07-31 23:36:00 -0400

    Removed URLs from !derpi
    The url function in DerPyBooru is currently broken and breaks the !derpi
    function. Commented out until resolved

--- derpi.py
+++ derpi.py
@@ -13,7 +13,7 @@ def search(query, highvoted=True):
 	i = next(s)
 	while highvoted and i.upvotes < 100:
 		i = next(s)
-	return i.full + '\n' + i.url
+	return i.full #+ '\n' + i.url	Python Wrapper functionality for .url is currently broken
 
 def process(message):
 	words = [s.strip() for s in message.content.lower().split(' ',maxsplit=1)]
