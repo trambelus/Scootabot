8ebe44448a761c457b85e4b0c86215c9bcdb7998
Trambelus <trambelus2@gmail.com>
2016-08-31 00:25:17 -0400

    Change a bunch of tabs to spaces

--- derpi.py
+++ derpi.py
@@ -7,32 +7,32 @@ import emotes
 import auth
 
 def search(query, highvoted=True):
-	print(query)
-	s = derpibooru.search.Search(key=auth.find_pw('derpibooru'), sf='random',
-		q=set(query))
-	i = next(s)
-	while highvoted and i.upvotes < 100:
-		i = next(s)
-	return i.full #+ '\n' + i.url	Python Wrapper functionality for .url is currently broken
+    print(query)
+    s = derpibooru.search.Search(key=auth.find_pw('derpibooru'), sf='random',
+        q=set(query))
+    i = next(s)
+    while highvoted and i.upvotes < 100:
+        i = next(s)
+    return i.full #+ '\n' + i.url    Python Wrapper functionality for .url is currently broken
 
 def process(message):
-	words = [s.strip() for s in message.content.lower().split(' ',maxsplit=1)]
-	if len(words) == 1:
-		tags = []
-	else:
-		tags = words[1].split(',')
-	if message.channel.is_private or "nsfw" not in message.channel.name:
-		tags.append("safe")
-	# Blacklisted tags
-	tags.append("-poop")
-	tags.append("-diaper")
-	tags.append("-urine")
-	try:
-		response = search(tags)
-		return response
-	except StopIteration:
-		try:
-			response = search(tags, highvoted=False)
-			return response
-		except StopIteration:
-			return emotes.get_message(emotes.NOPE)
+    words = [s.strip() for s in message.content.lower().split(' ',maxsplit=1)]
+    if len(words) == 1:
+        tags = []
+    else:
+        tags = words[1].split(',')
+    if message.channel.is_private or "nsfw" not in message.channel.name:
+        tags.append("safe")
+    # Blacklisted tags
+    tags.append("-poop")
+    tags.append("-diaper")
+    tags.append("-urine")
+    try:
+        response = search(tags)
+        return response
+    except StopIteration:
+        try:
+            response = search(tags, highvoted=False)
+            return response
+        except StopIteration:
+            return emotes.get_message(emotes.NOPE)
--- main.py
+++ main.py
@@ -26,156 +26,156 @@ EMAIL = 'hawke252.reddit@gmail.com'
 client = discord.Client()
 
 def restart(channel_id, force=False):
-	if os.name == 'nt':
-		logging.debug("NT restart")
-		subprocess.Popen(' '.join(["python", sys.argv[0], str(channel_id)]))
-		client.logout()
-		sys.exit(0)
+    if os.name == 'nt':
+        logging.debug("NT restart")
+        subprocess.Popen(' '.join(["python", sys.argv[0], str(channel_id)]))
+        client.logout()
+        sys.exit(0)
 
-	elif os.name == 'posix':
-		logging.debug("POSIX restart")
-		msg = git.cmd.Git('.').pull()
-		logging.debug("Git pull yielded {}".format(msg))
+    elif os.name == 'posix':
+        logging.debug("POSIX restart")
+        msg = git.cmd.Git('.').pull()
+        logging.debug("Git pull yielded {}".format(msg))
 
-		if not force and msg == 'Already up-to-date.':
-			return emotes.get_emote(emotes.NOPE) + ' ' + msg
-		else:
-			logging.debug("Logging out")
-			client.logout()
+        if not force and msg == 'Already up-to-date.':
+            return emotes.get_emote(emotes.NOPE) + ' ' + msg
+        else:
+            logging.debug("Logging out")
+            client.logout()
 
-			logging.debug("Restarting with args {}, {}".format(sys.argv[0], str(channel_id)))
-			sys.stdout.flush()
-			os.execl(sys.argv[0], sys.argv[0], str(channel_id))
+            logging.debug("Restarting with args {}, {}".format(sys.argv[0], str(channel_id)))
+            sys.stdout.flush()
+            os.execl(sys.argv[0], sys.argv[0], str(channel_id))
 
-	else:
-		logging.error("Unknown OS {}, could not restart".format(os.name))
+    else:
+        logging.error("Unknown OS {}, could not restart".format(os.name))
 
 class Command:
-	last_command = {}
+    last_command = {}
 
-	def __init__(self, message):
-		self.message = message
-		self.text = message.content.lower()
+    def __init__(self, message):
+        self.message = message
+        self.text = message.content.lower()
 
-	def process(self):
-		ret = None
+    def process(self):
+        ret = None
 
-		try:
-			if self.text == '!reload':
-				ret = restart(self.message.channel.id)
-				# Only returns this if a restart isn't happening
-				
-			elif self.text == '!force-reload':
-				restart(self.message.channel.id, force=True)
+        try:
+            if self.text == '!reload':
+                ret = restart(self.message.channel.id)
+                # Only returns this if a restart isn't happening
+                
+            elif self.text == '!force-reload':
+                restart(self.message.channel.id, force=True)
 
-			elif self.text == '!stop':
-				client.send_message(self.message.channel, emotes.get_message(emotes.BYE))
-				sys.exit(0)
+            elif self.text == '!stop':
+                client.send_message(self.message.channel, emotes.get_message(emotes.BYE))
+                sys.exit(0)
 
-			elif self.text.startswith('!derpi ') or self.text == '!derpi':
-				ret = derpi.process(self.message)
-				self.last_command[self.message.author.id + ':' + self.message.channel.id] = self.text
+            elif self.text.startswith('!derpi ') or self.text == '!derpi':
+                ret = derpi.process(self.message)
+                self.last_command[self.message.author.id + ':' + self.message.channel.id] = self.text
 
-			elif self.text == '!again':
-				if (self.message.author.id + ':' + self.message.channel.id) in self.last_command:
-					self.text = self.last_command[self.message.author.id + ':' + self.message.channel.id]
-					self.message.content = self.last_command[self.message.author.id + ':' + self.message.channel.id]
-					return self.process()
-				else:
-					ret = emotes.get_message(emotes.HUH)
+            elif self.text == '!again':
+                if (self.message.author.id + ':' + self.message.channel.id) in self.last_command:
+                    self.text = self.last_command[self.message.author.id + ':' + self.message.channel.id]
+                    self.message.content = self.last_command[self.message.author.id + ':' + self.message.channel.id]
+                    return self.process()
+                else:
+                    ret = emotes.get_message(emotes.HUH)
 
-			elif 'scootabot' in self.text:
+            elif 'scootabot' in self.text:
 
-				if 'roll' in self.text:
-					search_res = re.search(r'\d*d(\d+)(\s*\+\s*\d+)?', self.text)
-					if not search_res:
-						return
+                if 'roll' in self.text:
+                    search_res = re.search(r'\d*d(\d+)(\s*\+\s*\d+)?', self.text)
+                    if not search_res:
+                        return
 
-					roll = [1 if e == '' else int(e) for e in search_res.group(0).replace(' ','').replace('d','+').split('+')]
+                    roll = [1 if e == '' else int(e) for e in search_res.group(0).replace(' ','').replace('d','+').split('+')]
 
-					dice = [random.randint(1, roll[1]) for _ in range(roll[0])]
-					if len(roll) == 3:
-						dice += [roll[2]]
+                    dice = [random.randint(1, roll[1]) for _ in range(roll[0])]
+                    if len(roll) == 3:
+                        dice += [roll[2]]
 
-					if len(roll) == 2:
-						ret = "{}\n _Rolling {}d{}:_\n**{}**".format(
-							emotes.get_emote(emotes.YEP),
-							roll[0],
-							" + ".join(map(str,roll[1:])),
-							sum(dice)
-						)
-					else:	
-						ret = "{}\n _Rolling {}d{}:_\n  {}\n**={}**".format(
-							emotes.get_emote(emotes.YEP),
-							roll[0],
-							" + ".join(map(str,roll[1:])),
-							" + ".join(map(str,dice)),
-							sum(dice)
-						)
+                    if len(roll) == 2:
+                        ret = "{}\n _Rolling {}d{}:_\n**{}**".format(
+                            emotes.get_emote(emotes.YEP),
+                            roll[0],
+                            " + ".join(map(str,roll[1:])),
+                            sum(dice)
+                        )
+                    else:    
+                        ret = "{}\n _Rolling {}d{}:_\n  {}\n**={}**".format(
+                            emotes.get_emote(emotes.YEP),
+                            roll[0],
+                            " + ".join(map(str,roll[1:])),
+                            " + ".join(map(str,dice)),
+                            sum(dice)
+                        )
 
-				else:
-					ret = response.respond(self)
-				#elif 'thanks' in self.text:
-				#	ret = "{} {}".format(emotes.get_emote(emotes.YEP), "You're welcome!")
+                else:
+                    ret = response.respond(self)
+                #elif 'thanks' in self.text:
+                #    ret = "{} {}".format(emotes.get_emote(emotes.YEP), "You're welcome!")
 
-				#elif self.text[-1] == "!":
-				#	ret = emotes.get_message(emotes.YEP)
+                #elif self.text[-1] == "!":
+                #    ret = emotes.get_message(emotes.YEP)
 
-				#elif self.text[-1] == "?":
-				#	ret = emotes.get_message(emotes.NOPE)
+                #elif self.text[-1] == "?":
+                #    ret = emotes.get_message(emotes.NOPE)
 
-			# elif 'scootabot' in self.text and 'hawke' in self.text and 'favorite pon' in self.text:
-			# 	ret = emotes.get_emote(emotes.YEP) + ' Twist!'
+            # elif 'scootabot' in self.text and 'hawke' in self.text and 'favorite pon' in self.text:
+            #     ret = emotes.get_emote(emotes.YEP) + ' Twist!'
 
-			#if re.search(r'(^| )(hi|hello|hey|hi there|hiya|heya|howdy)(! |, | )scootabot', self.text):
-			#	author = re.search('(^| )\w+( ♀)?$', self.message.author.name).group().strip().title()
-			#	ret = emotes.get_message(emotes.HI, author)
+            #if re.search(r'(^| )(hi|hello|hey|hi there|hiya|heya|howdy)(! |, | )scootabot', self.text):
+            #    author = re.search('(^| )\w+( ♀)?$', self.message.author.name).group().strip().title()
+            #    ret = emotes.get_message(emotes.HI, author)
 
-		except SystemExit:
-			print("sys.exit called")
-			sys.exit(0)
+        except SystemExit:
+            print("sys.exit called")
+            sys.exit(0)
 
-		except:
-			exc = traceback.format_exc()
-			client.send_message(self.message.channel, "[](/notquitedashie) ```{}```".format(exc))
-			print(exc)
+        except:
+            exc = traceback.format_exc()
+            client.send_message(self.message.channel, "[](/notquitedashie) ```{}```".format(exc))
+            print(exc)
 
-		return ret
+        return ret
 
 
 @client.event
 def on_ready():
-	# Restart info should be in sys.argv
-	if len(sys.argv) == 3:
-		logging.info('Launched with client ID {}. Last code revision: {}'.format(*sys.argv[1:3]))
-	if len(sys.argv) > 1:
-		print(sys.argv)
-		channel = client.get_channel(sys.argv[1])
-		print(channel)
-		client.send_message(channel, emotes.get_message(emotes.HI, random.choice(["all","everyone","everybody","folks","guys","y'all"])))
-	logging.info("Ready!")
-	logging.debug("Launched with args {}".format(sys.argv))
+    # Restart info should be in sys.argv
+    if len(sys.argv) == 3:
+        logging.info('Launched with client ID {}. Last code revision: {}'.format(*sys.argv[1:3]))
+    if len(sys.argv) > 1:
+        print(sys.argv)
+        channel = client.get_channel(sys.argv[1])
+        print(channel)
+        client.send_message(channel, emotes.get_message(emotes.HI, random.choice(["all","everyone","everybody","folks","guys","y'all"])))
+    logging.info("Ready!")
+    logging.debug("Launched with args {}".format(sys.argv))
 
 @client.event
 def on_message(message):
-	if message.author != client.user:
-		logging.info(" {} said: {}".format(message.author, message.content))
+    if message.author != client.user:
+        logging.info(" {} said: {}".format(message.author, message.content))
 
-		cmd = Command(message=message)
-		response = cmd.process()
+        cmd = Command(message=message)
+        response = cmd.process()
 
-		if response:
-			client.send_message(message.channel, response)
+        if response:
+            client.send_message(message.channel, response)
 
 def main():
 
-	password = auth.find_pw(EMAIL)
+    password = auth.find_pw(EMAIL)
 
-	client.login(EMAIL, password)
-	client.run() # enter main loop
+    client.login(EMAIL, password)
+    client.run() # enter main loop
 
 if __name__ == '__main__':
-	main()
+    main()
 
 # TODO:
 # Keep search object handle for !again
