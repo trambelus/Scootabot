c448617cb6281ddbe6f852fc9611f76cf78be764
Hawke252 <hawkemaul16@yahoo.com>
2016-09-07 01:31:24 -0400

    Moved the next

--- main.py
+++ main.py
@@ -85,7 +85,7 @@ class Command:
                     ret = emotes.get_message(emotes.HUH)
 
             elif self.text == '!join':
-                next(yield from client.join_voice_channel(discord.Object(id='134973009910956033')))
+                yield from next(client.join_voice_channel(discord.Object(id='134973009910956033')))
 
             elif 'scootabot' in self.text:
 
