80aa3e4ea5509f0ef3ebb1325499d0ed48a2212f
Hawke252 <hawkemaul16@yahoo.com>
2016-09-07 01:28:53 -0400

    Does avoiding yield work?

--- main.py
+++ main.py
@@ -85,7 +85,7 @@ class Command:
                     ret = emotes.get_message(emotes.HUH)
 
             elif self.text == '!join':
-                yield from client.join_voice_channel(discord.Object(id='134973009910956033'))
+                client.join_voice_channel(discord.Object(id='134973009910956033'))
 
             elif 'scootabot' in self.text:
 
